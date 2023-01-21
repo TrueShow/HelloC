c81ea7bf2c5f101160689643167dbeb3e882d352
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 11:43:17 +0300

    add ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число, максимальное 999, минимальное 0");
+int number = int.Parse(Console.ReadLine());
+
+if (number / 100 > 0) {
+    Console.WriteLine($"Третье число от заданного = {number %10}");
+}
+else {
+    Console.WriteLine($"Нет третьего число в заданном числе {number}");
+}
