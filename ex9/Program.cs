03765a3abcdb67e70cbab8495f42aae8bffebb6f
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 11:39:01 +0300

    add ex9

---
+++ ex9/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите кратность");
+int secondNumber = int.Parse(Console.ReadLine());
+
+if (number % secondNumber == 0) {
+    Console.WriteLine($"Заданное число {number} кратное {secondNumber}");
+}
+else {
+    Console.WriteLine($"Остаток от заданного числа {number} = {number % secondNumber}");
+}
