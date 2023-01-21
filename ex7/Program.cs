58a5c94a85cc3ee8b55be98d070166bb642db42c
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 11:10:58 +0300

    add ex7

---
+++ ex7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число от 10 до 99");
+int number = int.Parse(Console.ReadLine());
+int max = 0;
+if (number < 10 || number > 99) {
+    Console.WriteLine("Неверное число, введите от 10 до 99");
+}
+else {
+    max = number / 10;
+    if (number % 10 > max) {
+        max = number % 10;
+    }
+}
+Console.WriteLine("Максимальное число: " + max);
