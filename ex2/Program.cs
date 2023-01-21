460ed4ccaa84445fe8e03299f97bb43507a1f09b
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 10:25:50 +0300

    add ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+int dayOfweek = int.Parse(Console.ReadLine());
+
+if (dayOfweek > 7 || dayOfweek < 1) {
+    Console.WriteLine("Неверный номер дня недели");
+}
+else {
+    switch (dayOfweek) {
+        case 1:
+            Console.WriteLine("понедельник");
+            break;
+        case 2:
+            Console.WriteLine("вторник");
+            break;
+        case 3:
+            Console.WriteLine("среда");
+            break;
+        case 4:
+            Console.WriteLine("четверг");
+            break;
+        case 5:
+            Console.WriteLine("пятница");
+            break;
+        case 6:
+            Console.WriteLine("суббота");
+            break;
+        case 7:
+            Console.WriteLine("воскресенье");
+            break;
+    }
+}
