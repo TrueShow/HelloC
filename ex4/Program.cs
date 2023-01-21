cf36ac1e4d874488c5a438a92fdccb9cf3cac29f
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 10:41:49 +0300

    add ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 1) {
+    Console.WriteLine("число должно быть положительным");
+}
+else {
+    for (int i = 1; i < number; i++){
+        if (i % 2 == 0){
+            Console.WriteLine(i);
+        }
+    }
+}
