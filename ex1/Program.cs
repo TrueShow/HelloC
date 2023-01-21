a893bcf468ab78c36c0db2f65ed47e5e69a1923b
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 10:12:19 +0300

    add ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число:");
+int firstNum = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secontNum = int.Parse(Console.ReadLine());
+
+if (secontNum * secontNum == firstNum) {
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else {
+    Console.WriteLine("Первое число не является квадратом второго");
+}
