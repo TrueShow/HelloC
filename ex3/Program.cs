dfa201b565639ef48ad99cb7caff5c07e25fee1f
Валерий Трушов <trueshow@MacBook-Air.local>
2023-01-21 10:34:29 +0300

    add ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число для проверки его на четность: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0) {
+    Console.WriteLine($"Число {number} - четное");
+}
+else {
+    Console.WriteLine($"Число {number} - нечетное");
+}
